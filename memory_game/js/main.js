24b4b5765b6c849466f9344cf212cb74516e6f99
Sheryll Villanueva <sheryllvillanueva@sherylls-mbp.attlocal.net>
2018-01-20 13:38:20 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,20 +2,19 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-//first card user flipped
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-//second card user flipped
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
+var checkForMatch = function (){
+	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
 }
 
+var flipCard = function (cardId) {
+	checkForMatch();
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push (cards[cardId]);
+}
+
+flipCard(0);
+flipCard(2);
