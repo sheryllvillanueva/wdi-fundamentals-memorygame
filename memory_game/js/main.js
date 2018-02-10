ab86957cf0fc130f8ae135abbe24474a402758ea
Sheryll Villanueva <sheryllvillanueva@sherylls-mbp.attlocal.net>
2018-02-09 21:41:39 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,26 +24,34 @@ var cards = [
 
 var cardsInPlay = [];
 
+var gameScore = 0;
+
 var checkForMatch = function (){
 	
 	if (cardsInPlay.length === 2  && cardsInPlay[0] === cardsInPlay[1]){
 		alert("You found a match!");
+		gameScore = gameScore + 1;
+		document.getElementById('score').innerHTML=gameScore;
 	} else {
 		alert("Sorry, try again.");
 	}
-};
+}
 
 var flipCard = function () {
 	var cardId = this.getAttribute('data-id');
 	this.setAttribute('src', cards[cardId].cardImage);
 	cardsInPlay.push (cards[cardId].rank);
+	console.log(cardsInPlay.length);
 	if (cardsInPlay.length === 2){
 	checkForMatch();
-	};
+	} else if (cardsInPlay.length > 2){
+		resetcard();
+	}
+
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-};
+}
 
 var createBoard = function () {
 	for (var i=0; i<cards.length; i++){	
@@ -54,7 +62,26 @@ var createBoard = function () {
 		document.getElementById('game-board').appendChild(cardElement);
 
 	}
-};
+
+}
+
+var resetcard = function () {
+	document.getElementById('game-board').innerHTML = "";
+	createBoard();
+	cardsInPlay = [];
+}
+
+
 
 createBoard();
+
+document.getElementById('resetBtn').addEventListener('click', resetcard);
+
+
+
+
+
+
+
+
 
